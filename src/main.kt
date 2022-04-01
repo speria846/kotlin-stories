21bb62b3f84251e2195205ff1d92909de388a936
speria846 <niwenyesigasperia@gmail.com>
2022-04-01 15:53:02 +0300

    fifth commit

---
+++ src/main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    story()
+    seem("Kibwetere and his group come and burnt the whole church and that marked the end of their lives in this world.",8)
+    }
+fun story(){
+    val c="long time ago there was a man called Kibwetere,he lived in kanungu district,uganda."
+    val m="one day kibwetere formed a church of believers and became the master but his mission was to take people properties kill them"
+    val b="one sunday morning,he called his believers to come and pray that becouse the Lord was soon coming"
+  println(c+m+b)
+}
+    fun seem(q:String,a:Int){
+     println("so people came and started praying and preparing to see God, and then after $a hours in the church $q")
+      }
+
+
+
